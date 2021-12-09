171cee988512b56df5022bc7cd053891cd254ba1
henryaugusta <henryaugusta4@gmail.com>
2021-12-10 01:48:18 +0700

    provide data format for android

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,16 +2,26 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
     use HasFactory;
-    protected $appends = ['photo_path'];
 
-    function getPhotoPathAttribute(){
+    protected $appends = ['photo_path', 'date_indo'];
+
+    function getPhotoPathAttribute()
+    {
         return asset($this->photo);
+    }
+
+    function getDateIndoAttribute()
+    {
+        $dbDate = Carbon::createFromFormat('Y-m-d H:i:s', $this->created_at)
+            ->locale('id')->isoFormat('dddd, D MMMM Y');
+        return $dbDate;
     }
 
     protected $casts = [
